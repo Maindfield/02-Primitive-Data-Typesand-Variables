4d7ab3e3525bb6c5daf815cfc01193e0fcf53dbb
maindfield <rizov.g@gmail.com>
2015-01-17 10:51:11 +0200

    Homework 02

---
+++ 01DeclareVariables/01DeclareVariables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DeclareVariables
+
+         //Homework: Primitive Data Types and Variables
+         //Problem 1. Declare Variables
+         //Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, 
+         //short, ushort, int, uint, long, ulong to represent the following values: 52130, -115, 4825932, 97, -10000.
+         //Choose a large enough type for each number to ensure it will fit in it. Try to compile the code.
+
+{
+    class DeclareVariables
+    {
+        static void Main()
+        {
+            byte firstNumber = 97; 
+            sbyte secondNumber = -115;
+            short thirdNumber = -10000;
+            int fourthNumber  = 52130;
+            ulong fifthNumber = 4825932;
+
+            Console.WriteLine("byte is = {0}\n sbyte is = {1}\n short is = {2}\n int is = {3}\n ulong is = {4}\n",
+                               firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+        }
+    }
+}
---
+++ 05BooleanVariable/05BooleanVariable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _BooleanVariable
+
+                       //Problem 5. Boolean Variable
+                       //Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.
+                       //Print it on the console.
+
+{
+    class BooleanVariable
+    {
+        static void Main()
+        {
+            bool isFemale = false;
+            Console.WriteLine("Is my gender Female? = {0}", isFemale);
+        }
+    }
+}
